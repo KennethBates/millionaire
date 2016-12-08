3948aa974b80da8d97b76e967c3052c364ef769d
Rob Pearson <robpearson@fastmail.net>
2016-12-08 11:10:09 +1000

    Created new webapp.

---
+++ KennyAndRob.Millionaire/KennyAndRob.Millionaire/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KennyAndRob.Millionaire.Startup))]
+namespace KennyAndRob.Millionaire
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

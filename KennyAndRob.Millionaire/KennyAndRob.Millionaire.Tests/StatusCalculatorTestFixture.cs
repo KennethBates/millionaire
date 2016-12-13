223a852c1ddbb4ac5ba2f4deee089c8fd98401ef
Rob Pearson <robpearson@fastmail.net>
2016-12-14 09:47:48 +1000

    Added initial tests.

---
+++ KennyAndRob.Millionaire/KennyAndRob.Millionaire.Tests/StatusCalculatorTestFixture.cs
@@ -0,0 +1,35 @@
+﻿using KennyAndRob.Millionaire.Models;
+using NUnit.Framework;
+
+namespace KennyAndRob.Millionaire.Tests
+{
+    [TestFixture]
+    public class StatusCalculatorTestFixture
+    {
+        [Test]
+        public void Can_Calculate_Status()
+        {
+            // Given
+            var netWorth = 20; // $20
+
+            // When
+            var status = StatusCalculator.HowRichAmI(netWorth);
+
+            // Then
+            Assert.That(status, Is.EqualTo("You are poor"));
+        }
+
+        [Test]
+        public void Can_Calculate_Status_That_I_Am_Rich()
+        {
+            // Given
+            var netWorth = 20000000; // 20 million
+
+            // When
+            var status = StatusCalculator.HowRichAmI(netWorth);
+
+            // Then
+            Assert.That(status, Is.EqualTo("You are crazy rich!!!"));
+        }
+    }
+}
---
+++ KennyAndRob.Millionaire/KennyAndRob.Millionaire/Models/StatusCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KennyAndRob.Millionaire.Models
+{
+    public class StatusCalculator
+    {
+        public static string HowRichAmI(int netWorth)
+        {
+            var status = "You are poor";
+
+            if (netWorth > 10000000)
+            {
+                status = "You are crazy rich!!!";
+            }
+            else if (netWorth > 1000000)
+            {
+                status = "You are filthy rich!";
+            }
+
+            return status;
+        }
+    }
+}
